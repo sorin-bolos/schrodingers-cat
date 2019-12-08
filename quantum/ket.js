7fa0c8c8f829321e7323dd96161ae594ec2bd1e1
Sorin Bolos <sorin.bolos@uipath.com>
2019-12-08 09:16:43 +0200

    Added tensor for ket

--- quantum/ket.js
+++ quantum/ket.js
@@ -1,18 +1,37 @@
+const math = require('mathjs')
+const PRECISION = 3;
+
 class Ket {
-    constructor(_alpha, _beta) {
+    constructor(_amplitudes) {
+        let vector = _amplitudes.map(a => typeof a == 'number' ? a.toFixed(PRECISION) : a)
+                                .map(a => new math.complex(a))
 
-        if (!this.isUnitary(_alpha, _beta))
-            throw new Exception("Ket must be unitary")
+        if (!this.isUnitary(vector))
+            throw `Ket must be unitary (${vector})`;
 
-        this.vector = [_alpha, _beta];
+        this.amplitudes = vector;
     }
 
-    isUnitary(alpha, beta) {
-        let modulus = alpha * alpha.conjugate() + beta * beta.conjugate()
-        return Math.abs(modulus - 1) < Number.EPSILON
+    isUnitary(vector) {
+        let sum = vector.reduce((total, element) => total + element * element.conjugate(), 0)
+                        .toFixed(PRECISION);
+        return sum == 1;
     }
+
+    tensor(otherKet) {
+        let tensored = this.amplitudes.map(a => otherKet.amplitudes.map(b => a*b))
+                                      .reduce((v1, v2) => v1.concat(v2));
+        return new Ket(tensored);
+    } 
 }
 
+const Zero = new Ket([1, 0]);
+const One = new Ket([0, 1]);
+const Plus = new Ket([1/math.sqrt(2), 1/math.sqrt(2)]);
+
 module.exports = {
-    Ket
-  }
+    Ket,
+    Zero,
+    One,
+    Plus
+}
--- test/ket.test.js
+++ test/ket.test.js
@@ -1,28 +1,58 @@
 const math = require('mathjs')
 const k = require('../quantum/ket')
+const assert = require('assert');
 
-var assert = require('assert');
 describe('Ket', function() {
   describe('#isUnitary()', function() {
     it('[0, 1] should be unitary', function() {
-      let ket = new k.Ket(math.complex(0) ,math.complex(1))
-      assert.equal(ket.isUnitary(math.complex(0) ,math.complex(1)), true);
+      let ket = new k.Ket([0, 1])
+      assert.ok(ket);
     });
 
     it('[1, 0] should be unitary', function() {
-      let ket = new k.Ket(math.complex(1) ,math.complex(0))
-      assert.equal(ket.isUnitary(math.complex(1) ,math.complex(0)), true);
+      let ket = new k.Ket([1, 0]);
+      assert.ok(ket);
     });
 
     it('[1/sqrt(2), 1/sqrt(2)] should be unitary', function() {
-      let c = math.complex(1/math.sqrt(2));
-      let r = Math.abs(c*c.conjugate() - 1/2)
-
-      assert.equal(r < Number.EPSILON, true);
+      let ket = new k.Ket([1/math.sqrt(2), 1/math.sqrt(2)])
+      assert.ok(ket);
     });
 
     it('[1, 1] should not be unitary', function() {
-      assert.throws(() => new k.Ket(math.complex(1) ,math.complex(1)), "Ket must be unitary");
+      assert.throws(() => new k.Ket([1, 1]));
+    });
+  });
+
+  describe('#tensor(otherKet)', function() {
+    it('[1, 0] tensor [1, 0] should be [1, 0, 0, 0]', function() {
+      let zeroZero = k.Zero.tensor(k.Zero);
+      let expected = [1, 0, 0, 0].map(e => new math.complex(e));
+      assert.deepEqual(zeroZero.amplitudes, expected);
+    });
+
+    it('[1, 0] tensor [0, 1] should be [0, 1, 0, 0]', function() {
+      let zeroOne = k.Zero.tensor(k.One);
+      let expected = [0, 1, 0, 0].map(e => new math.complex(e));
+      assert.deepEqual(zeroOne.amplitudes, expected);
+    });
+
+    it('[0, 1] tensor [1, 0] should be [0, 0, 1, 0]', function() {
+      let oneZero = k.One.tensor(k.Zero);
+      let expected = [0, 0, 1, 0].map(e => new math.complex(e));
+      assert.deepEqual(oneZero.amplitudes, expected);
+    });
+
+    it('[0, 1] tensor [0, 1] should be [0, 0, 0, 1]', function() {
+      let oneOne = k.One.tensor(k.Zero);
+      let expected = [0, 0, 1, 0].map(e => new math.complex(e));
+      assert.deepEqual(oneOne.amplitudes, expected);
+    });
+
+    it('[1/sqrt(2), 1/sqrt(2)] tensor [1/sqrt(2), 1/sqrt(2)] should be [1/2, 1/2, 1/2, 1/2]', function() {
+      let ket = k.Plus.tensor(k.Plus);
+      let expected = [1/2, 1/2, 1/2, 1/2].map(e => new math.complex(e));
+      assert.deepEqual(ket.amplitudes, expected);
     });
   });
 });
